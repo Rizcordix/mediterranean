b092337936d754bb9c8f1d064e47dd395ff11455
Murtaza445 <MurtazaJohar123@gmail.com>
2026-01-12 14:40:17 +0500

    metapixelscriptadded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,12 @@ import { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import WidgetClientWrapper from '@/components/wrappers/WidgetWrapper'
 import { PopupProvider } from '@/components/wrappers/PopupContext'
-import PopupClientWrapper from "@/components/popup/popupclient";
+import PopupClientWrapper from "@/components/popup/popupclient"
 import Script from 'next/script'
 import { DiscountPopupProvider } from '@/components/popup/DiscountPopupContext'
 
 import 'aos/dist/aos.css'
 import '@/assets/scss/style.scss'
-// import { DEFAULT_PAGE_TITLE } from '@/context/constants'
 
 const inter = Poppins({
   subsets: ['latin'],
@@ -26,34 +25,62 @@ export const metadata: Metadata = {
   },
 }
 
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
         <style suppressHydrationWarning>{}</style>
+
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1406590091186942');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
+
       <body className={inter.className}>
+        {/* Meta Pixel NoScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1406590091186942&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <div id="__next_splash">
           <DiscountPopupProvider>
-          <PopupProvider>
-            <AppProviders>{children}</AppProviders>
-            <Script
-          src="https://embed.tawk.to/6954e038d5d3bd197b4d818f/1jdponnti"
-          strategy="afterInteractive"
-          async
-        />
-          </PopupProvider>
+            <PopupProvider>
+              <AppProviders>{children}</AppProviders>
+
+              {/* Tawk.to Chat */}
+              <Script
+                src="https://embed.tawk.to/6954e038d5d3bd197b4d818f/1jdponnti"
+                strategy="afterInteractive"
+                async
+              />
+            </PopupProvider>
           </DiscountPopupProvider>
         </div>
-          <PopupClientWrapper />
+
+        <PopupClientWrapper />
         <WidgetClientWrapper />
       </body>
     </html>
   )
 }
-
