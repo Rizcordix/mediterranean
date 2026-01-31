a509be89e82c7e2878c7357f468955cb637060b7
Murtaza445 <MurtazaJohar123@gmail.com>
2026-01-31 21:19:16 +0500

    smallchange

--- src/components/popup/popupclient.tsx
+++ src/components/popup/popupclient.tsx
@@ -1,5 +1,5 @@
 "use client";
 import PopupCard from "./popup";
 export default function PopupClientWrapper() {
-  return <PopupCard showOnEveryVisit={false} delayMs={10000} />;
+  return <PopupCard showOnEveryVisit={true} delayMs={10000} />;
 }
