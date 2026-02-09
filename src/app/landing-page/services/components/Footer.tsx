1acc974a64c06caea969289cb2b45cbfab1652c8
Murtaza445 <MurtazaJohar123@gmail.com>
2026-02-10 02:50:56 +0500

    somesmallfooterupdate

--- src/app/landing-page/services/components/Footer.tsx
+++ src/app/landing-page/services/components/Footer.tsx
@@ -111,18 +111,18 @@ const Footer = () => {
                   </svg>
                   <span>6340 N Eldridge Pkwy suite N, Houston, Texas 77041, United States</span>
                 </li>
-                {/* <li>
+                <li>
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                   </svg>
-                  <a href="tel:+442038787938">+44 20 3878 7938</a>
-                </li> */}
-                <li>
+                  <a href="tel:+12812470786">+1 281-247-0786</a>
+                </li>
+                {/* <li>
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <path d="M22" />
                   </svg>
                   <a href="tel:+12812470786">+1 281-247-0786</a>
-                </li>
+                </li> */}
                 <li>
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
